a147c26051aa59d5f5333f040519ec85dcacbce1
Guillermo Robles, Jr <will.robles@gmail.com>
2015-02-11 13:19:39 -0600

    added requirejs module

---
+++ src/requirejs/module.js
@@ -0,0 +1,15 @@
+/**
+ * Require.js Boilerplate Module
+ */
+define(function(require){
+    
+    'use strict';
+
+    var _ = require('lodash');
+
+    var module = function(){
+
+    };
+
+    return module;
+});
