caec36e2d961ce32da50fa65bfafc64b6e96dffc
Guillermo Robles, Jr <will.robles@gmail.com>
2015-02-12 16:51:48 -0600

    changed the exmaple, added more functionality
    All classes inherit from Class.js. Added useful methods 'getAllKeys,
    getAllProperties, getAllMethods

--- src/javascript-inheritance/Class-example.js
+++ src/javascript-inheritance/Class-example.js
@@ -43,29 +43,13 @@
   };
   Class.prototype = Object.create(Object.prototype);
   Class.prototype.constructor = Class;
-  
   Class.prototype.make = function make(fn, parent){
     parent = parent || Object;
     var insFn = new fn();
     // everything will come from Class.prototype
     if (parent.name === 'Object') {
-      var x = (function(Class, fn){
-        // var child = function(){
-        //   Object.call(this);
-        //   return this;
-        // };
-        // child.prototype = Object.create(Object.prototype);
-        // child.prototype.constructor = child;
-        // child.prototype.doSomething = function doSomething(){};
-        // console.log(new child());
-        
-        // Create a class
-        // console.log(fn, Class);
-        // var ClassX = new Class();
-        // var x = ClassX.make(fn, Class);
-        // fn = x;
-        // console.log(fn, Class);
-        // 
+      var newParent = (function(Class, fn){
+
         var ClassY = new Class();
         var ClassX = ClassY.make(fn, Class);
         parent = ClassX;
@@ -77,22 +61,12 @@
         child.prototype = Object.create(Object.prototype);
         child.prototype.constructor = child;
 
-        // add useful methods
-        child.prototype.getAllKeys = function getAllKeys(){
-          var keys = [];
-          for (var key in this) {
-              keys.push(key);
-          }
-          return keys;
-        };
         return child;
       })(Class, fn);
-        var ClassY = new Class();
-        var ClassX = ClassY.make(x, Class);
-        
-      x.prototype = Object.create(ClassX.prototype);
-      x.prototype.constructor = fn;
-      return x;
+       
+      newParent.prototype = Object.create(Class.prototype);
+      newParent.prototype.constructor = fn;
+      return newParent;
     };
 
 
@@ -123,52 +97,161 @@
 
     return child;
   };
-
+  // Useful methods
+  Class.prototype.getAllKeys = function getAllKeys(){
+    var keys = [];
+    for (var key in this) {
+        keys.push(key);
+    }
+    return keys;
+  };
+  Class.prototype.getAllProperties = function getAllProperties(){
+    var properties = {};
+    var keys = this.getAllKeys();
+    for (var i = 0, len = keys.length; i < len; i++) {
+      var key = keys[i];
+      var keyType = typeof this[key];
+      if (keyType !== 'function') {
+        properties[key] = this[key];
+      };
+    };;
+    return properties;
+  };
+  Class.prototype.getAllMethods = function getAllMethods(){
+    var properties = {};
+    var keys = this.getAllKeys();
+    for (var i = 0, len = keys.length; i < len; i++) {
+      var key = keys[i];
+      var keyType = typeof this[key];
+      if (keyType === 'function') {
+        properties[key] = this[key];
+      };
+    };;
+    return properties;
+  };
+  Class.prototype.getPropertiesFrom = function getPropertiesFrom(){
+    //AnimalClass.prototype.isPrototypeOf(dog);
+  };
   // Attach to the window
   window.Class = Object.create(Class.prototype);
 })(window);
 
 // Example Usage
 (function(window, undefined){
-
-  'use strict';
-
-  // Create a class
-  var AnimalClass = window.AnimalClass = Class.make(function AnimalClass(){
-    this.animalThing = 2;
-    this.animalFunctionToBeInherited = function(){
-      console.log('I am added to the prototype!');
-    };
-    return this;
-  });
   
-  // Manually adding things to the AnimalClass prototype
-  AnimalClass.prototype.numberOfEyes = 2;
+  'use strict';
+  
+  var example1 = function(){
+    // Create a class
+    var AnimalClass = window.AnimalClass = Class.make(function AnimalClass(){
+      this.animalThing = 2;
+      this.animalFunctionToBeInherited = function(){
+        console.log('I am added to the prototype!');
+      };
+      return this;
+    });
+    
+    // Manually adding things to the AnimalClass prototype
+    AnimalClass.prototype.numberOfEyes = 2;
 
-  // Create a class
-  var DogClass = window.DogClass = Class.make(function DogClass(){
-    this.dogThing = 4;
-    return this;
-  }, AnimalClass);
+    // Create a class
+    var DogClass = window.DogClass = Class.make(function DogClass(){
+      this.dogThing = 4;
+      return this;
+    }, AnimalClass);
 
-  // Create the instances
-  var dog1 = new DogClass();
-  var dog2 = Object.create(DogClass.prototype);
-  //console.log(new AnimalClass(), Object.create(AnimalClass.prototype));
-  console.log(dog1, dog2);
+    // Create the instances
+    var dog1 = new DogClass();
+    var dog2 = Object.create(DogClass.prototype);
+    //console.log(new AnimalClass(), Object.create(AnimalClass.prototype));
+    console.log(dog1, dog2);
 
-  // Add things to the instance
-  dog1.addedDogThing = true;
-  dog1.animalThing = -5;
+    // Add things to the instance
+    dog1.addedDogThing = true;
+    dog1.animalThing = -5;
 
-  dog2.addedDogThing = 'stuff goes here';
-  dog2.animalThing = -69;
+    dog2.addedDogThing = 'stuff goes here';
+    dog2.animalThing = -69;
 
-  console.log(dog1, dog2);
-  console.log(new DogClass(), Object.create(DogClass.prototype));
-  // console.log(dog.animalNonProtoFunction());
-  // AnimalClass.prototype.isPrototypeOf(dog);
-  // Object.getPrototypeOf(dog)
-  // Object.keys(dog)
+    console.log(dog1, dog2);
+    console.log(new DogClass(), Object.create(DogClass.prototype));
+    // console.log(dog.animalNonProtoFunction());
+    // AnimalClass.prototype.isPrototypeOf(dog);
+    // Object.getPrototypeOf(dog)
+    // Object.keys(dog)
+  };
+
+  var example2 = function(){
+    // Create a class
+    var AnimalClass = window.AnimalClass = Class.make(function AnimalClass(){
+      this.animalThing = 2;
+      this.animalFunctionToBeInherited = function(){
+        console.log('I am added to the prototype!');
+      };
+      return this;
+    });
+    
+    // Manually adding things to the AnimalClass prototype
+    AnimalClass.prototype.numberOfEyes = 2;
+
+    var animal = new AnimalClass();
+    
+    var DogClass = animal.make(function DogClass(){
+      this.doggieThing = 503;
+      return this;
+    }, AnimalClass);
+
+    // var doggie = new DogClass();
+    var doggie = Object.create(DogClass.prototype);
+
+    var AwesomeDoggieClass = doggie.make(function AwesomeDoggieClass(){
+      this.awesomeDoggieThing = 2000;
+      return this;
+    }, DogClass);
+
+    // var awesomeDoggie = new AwesomeDoggieClass();
+    var awesomeDoggie = Object.create(AwesomeDoggieClass.prototype);
+
+    var SuperDoggieClass = awesomeDoggie.make(function SuperDoggieClass(){
+      this.superDoggieThing = 9500;
+      return this;
+    }, AwesomeDoggieClass);
+
+    // var superDoggie = new SuperDoggieClass();
+    var superDoggie = window.superDoggie = Object.create(SuperDoggieClass.prototype);
+
+    console.log(doggie, awesomeDoggie, superDoggie);
+
+    // // Create a class
+    // var DogClass = window.DogClass = Class.make(function DogClass(){
+    //   this.dogThing = 4;
+    //   return this;
+    // }, AnimalClass);
+
+    // // Create the instances
+    // var dog1 = new DogClass();
+    // var dog2 = Object.create(DogClass.prototype);
+    // //console.log(new AnimalClass(), Object.create(AnimalClass.prototype));
+    // console.log(dog1, dog2);
+
+    // // Add things to the instance
+    // dog1.addedDogThing = true;
+    // dog1.animalThing = -5;
+
+    // dog2.addedDogThing = 'stuff goes here';
+    // dog2.animalThing = -69;
+
+    // console.log(dog1, dog2);
+    // console.log(new DogClass(), Object.create(DogClass.prototype));
+    // console.log(dog.animalNonProtoFunction());
+    // AnimalClass.prototype.isPrototypeOf(dog);
+    // Object.getPrototypeOf(dog)
+    // Object.keys(dog)
+  };
+
+  example2();
 
 })(window);
+
+
+
