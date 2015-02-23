67d92987283077ee986aeda7ffc834b0d38da341
Guillermo Robles, Jr <will.robles@gmail.com>
2015-02-23 13:41:16 -0600

    added basic files

---
+++ src/js-testing/Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  // Initialize config.
+  grunt.initConfig({
+    pkg: require('./package.json'),
+  });
+
+  // Load per-task config from separate files.
+  grunt.loadTasks('grunt');
+
+  // Register alias tasks.
+  grunt.registerTask('dev',
+    'Start a live-reloading dev webserver on localhost.',
+    ['jshint', 'clean', 'connect:dev', 'watch']);
+
+  grunt.registerTask('default', ['dev']);
+
+};
